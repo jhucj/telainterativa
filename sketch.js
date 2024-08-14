a8344f45c4117da3f1c4e0be06b5b591251d7a76
jhucj <conceicaopiresjhulia@gmail.com>
2024-08-14 10:17:25 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,112 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+}function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("purple");
+}function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("purple");
+    rect(0, 0, 100, 150)
+}function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("white");
+
+    fill("pink")
+    rect(0, 0, 100, 150)
+}function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("purple");
+
+    stroke("purple")
+    fill("red")
+    rect(0, 0, 100, 150)
+}function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("purple");
+
+  stroke("blue")
+  fill("red")
+  rect(100, 250, 100, 150)
+}function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("purple");
+
+  stroke("brown")
+  fill("red")
+  rect(mouseX, mouseY, 20, 35)
+}function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("purple");
+
+  stroke("blue")
+  fill("red")
+  
+  console.log(mouseIsPressed)
+  rect(mouseX, mouseY, 20, 35)
+}function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("purple");
+
+  stroke("blue")
+  fill("black")
+  
+  // console.log(mouseIsPressed)
+  rect(mouseX, mouseY, 20, 35)
+}function setup() {
+  createCanvas(600, 600);
+}
+
+function draw() {
+  background("purple");
+
+  stroke("blue")
+  fill("red")
+  
+  // console.log(mouseIsPressed)
+  
+  if(mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35)
+  } 
+}function setup() {
+  createCanvas(600, 600);
+  background("purple");
+}
+
+function draw() {
+  stroke("blue");
+  fill("red");
+
+  // console.log(mouseIsPressed)
+
+  if (mouseIsPressed) {
+    rect(mouseX, mouseY, 20, 35);
+  }
+}
